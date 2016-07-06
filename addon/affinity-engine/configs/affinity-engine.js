8e57df5c0ac1fc8be350295541c9646b4535913e
null null null <null@revolution.cheap>
2016-07-06 16:42:08 -0700

    alphabetize config

--- addon/affinity-engine/configs/affinity-engine.js
+++ addon/affinity-engine/configs/affinity-engine.js
@@ -2,19 +2,21 @@ export default {
   priority: 0,
   globals: {
     classNames: ['et-paper', 'et-block'],
-    transitionDuration: 200,
     cps: 25,
-    tweenEffect: {
-      opacity: {
-        from: 0,
-        to: 1
-      }
+    keys: {
+      accept: ['Space', 'Enter'],
+      cancel: ['Escape'],
+      moveDown: ['ArrowDown', 'KeyS', 'Numpad2'],
+      moveLeft: ['ArrowLeft', 'KeyA', 'Numpad4'],
+      moveRight: ['ArrowRight', 'KeyD', 'Numpad6'],
+      moveUp: ['ArrowUp', 'KeyW', 'Numpad8']
     },
-    tweenRate: 25,
+    menuColumns: 1,
     transition: {
       duration: 250,
       effect: { opacity: 1 }
     },
+    transitionDuration: 200,
     transitionIn: {
       duration: 250,
       effect: { opacity: [1, 0] }
@@ -23,14 +25,13 @@ export default {
       duration: 250,
       effect: { opacity: 0 }
     },
-    keys: {
-      accept: ['Space', 'Enter'],
-      cancel: ['Escape'],
-      moveDown: ['ArrowDown', 'KeyS', 'Numpad2'],
-      moveLeft: ['ArrowLeft', 'KeyA', 'Numpad4'],
-      moveRight: ['ArrowRight', 'KeyD', 'Numpad6'],
-      moveUp: ['ArrowUp', 'KeyW', 'Numpad8']
-    }
+    tweenEffect: {
+      opacity: {
+        from: 0,
+        to: 1
+      }
+    },
+    tweenRate: 25
   },
   preloader: {
     path: 'service:affinity-engine/stubs/preloader'
