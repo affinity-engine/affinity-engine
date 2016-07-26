93e7fb754f48c0690a251af1131dda4d6e6db153
null null null <null@affinitygamescollective.org>
2016-07-26 07:51:19 -0700

    restructure configs

--- addon/affinity-engine/configs/affinity-engine.js
+++ addon/affinity-engine/configs/affinity-engine.js
@@ -1,15 +1,13 @@
 export default {
   priority: 0,
-  globals: {
-    cps: 25,
-    keys: {
-      accept: ['Space', 'Enter'],
-      cancel: ['Escape'],
-      moveDown: ['ArrowDown', 'KeyS', 'Numpad2'],
-      moveLeft: ['ArrowLeft', 'KeyA', 'Numpad4'],
-      moveRight: ['ArrowRight', 'KeyD', 'Numpad6'],
-      moveUp: ['ArrowUp', 'KeyW', 'Numpad8']
-    },
-    menuColumns: 1
-  }
+  cps: 25,
+  keys: {
+    accept: ['Space', 'Enter'],
+    cancel: ['Escape'],
+    moveDown: ['ArrowDown', 'KeyS', 'Numpad2'],
+    moveLeft: ['ArrowLeft', 'KeyA', 'Numpad4'],
+    moveRight: ['ArrowRight', 'KeyD', 'Numpad6'],
+    moveUp: ['ArrowUp', 'KeyW', 'Numpad8']
+  },
+  menuColumns: 1
 };
