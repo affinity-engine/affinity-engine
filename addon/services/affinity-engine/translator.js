0d718ddd07514425e2eea826989d71111bd42115
null null null <null@affinitygamescollective.org>
2016-08-05 08:35:48 -0700

    update translator stub

--- addon/services/affinity-engine/translator.js
+++ addon/services/affinity-engine/translator.js
@@ -7,7 +7,17 @@ const {
 
 export default Service.extend({
   locales: [],
+
+  exists() { return false; },
   setLocale: K,
+
+  formatDate(date) {
+    return date;
+  },
+
+  formatNumber(number) {
+    return number;
+  },
 
   translate(string) {
     return string;
