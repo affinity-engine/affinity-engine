91baa7c9f94edeb6ddea08dc87fc58da3d26bf75
null null null <null@affinitygamescollective.org>
2016-10-25 09:03:11 -0700

    add resize mixin

--- addon/mixins/window-resize.js
+++ addon/mixins/window-resize.js
@@ -1,30 +1,9 @@
 import Ember from 'ember';
+import ResizeAware from 'ember-resize/mixins/resize-aware';
 
-const {
-  Evented,
-  Mixin,
-  on
-} = Ember;
+const { Mixin } = Ember;
+const { inject: { service } } = Ember;
 
-const { run: { debounce } } = Ember;
-
-export default Mixin.create(Evented, {
-  _oldWindowHeight: null,
-  _oldWindowWidth: null,
-
-  _destroyResizeHandler: on('willDestroyElement', function() {
-    Ember.$(window).off(`resize.${this.elementId}`);
-  }),
-
-  _initResizeHandler: on('didInsertElement', function() {
-    const debounceDuration = 10;
-
-    Ember.$(window).on(`resize.${this.elementId}`, () => {
-      debounce(this, this._triggerResizeListeners, debounceDuration);
-    });
-  }),
-
-  _triggerResizeListeners() {
-    this.trigger('windowResize');
-  }
+export default Mixin.create(ResizeAware, {
+  resizeService: service('resize')
 });
