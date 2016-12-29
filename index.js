61f692a11d0e595e1be4a31fd7eda55ca7645588
null null null <null@affinitygamescollective.org>
2016-12-29 13:43:21 -0800

    update ember cli

--- index.js
+++ index.js
@@ -2,13 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'affinity-engine',
-
-  included: function(app) {
-    this._super.included(app);
-  },
-
-  safeIncluded: function(app, parent) {
-    this.included(app, parent);
-  }
+  name: 'affinity-engine'
 };
